7d0b9e19730708d89d5f3877a169815015ff65ce
miguel <miguel@MikeUbuntu>
2015-06-14 00:59:59 -0600

    modificado personalDisponibleBean

---
+++ src/java/com/sherwinca/actions/altasBajasBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sherwinca.actions;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Solis
+ */
+@ManagedBean
+@ViewScoped
+public class altasBajasBean {
+
+    /**
+     * Creates a new instance of altasBajasBean
+     */
+    public altasBajasBean() {
+    }
+    
+}
--- src/java/com/sherwinca/actions/personalDisponibleBean.java
+++ src/java/com/sherwinca/actions/personalDisponibleBean.java
@@ -3,88 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.sherwinca.actions;
-
 
 import com.sherwinca.entidades.Disponible;
 import com.sherwinca.entidades.HibernateUtil;
 import com.sherwinca.entidades.SigPersonaldisponible;
 import java.io.Serializable;
-
 import java.util.ArrayList;
-
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-
 /**
  *
- * @author Miguel
+ * @author miguel
  */
 @ManagedBean
 @ViewScoped
 public class personalDisponibleBean implements Serializable{
-    
-    private List<Disponible> lista = new ArrayList();
-    private String vcMes; /*CAPTURA LA SELECCION DEL COMBOBOX MES DE TACTICO.XHTML*/
-    private int iAnio; /*CAPTURA LA SELECCION DEL COMBOBOX ANIO DE TACTICO.XHTML*/
-    
-     public personalDisponibleBean() { /*CONSTRUCTOR*/
+
+    private List<Disponible> lista;
+    private String mes;
+    private Integer anio;
+
+    /**
+     * Creates a new instance of personalDisponibleBean
+     */
+    public personalDisponibleBean()  {
+        lista = new ArrayList<Disponible>();
     }
 
-    public List<Disponible> getLista() {
-        return lista;
-    }
-
-    public void setLista(List<Disponible> lista) {
-        this.lista = lista;
-    }
-
-    public String getVcMes() {
-        return vcMes;
-    }
-
-    public void setVcMes(String vcMes) {
-        this.vcMes = vcMes;
-    }
-
-    public int getiAnio() {
-        return iAnio;
-    }
-
-    public void setiAnio(int iAnio) {
-        this.iAnio = iAnio;
-    }
-
-    
-     
-     
-    
- 
-    public void listar(){
+    public void listar() {
         Session session = null;
         List<SigPersonaldisponible> list = null;
-  
-        try {            
+
+        try {
             session = HibernateUtil.getSessionFactory().openSession();
-            Query query = session.createQuery("from SigPersonaldisponible r where r.vc_mes_disponible like'"+vcMes+"' AND r.i_anio_disponible ='"+iAnio+"'" );            
+            Query query = session.createQuery("from SigPersonaldisponible r where r.vcMesDisponible=:mes AND r.iAnioDisponible =:anio");
+            query.setParameter("mes", mes);
+            query.setParameter("anio", anio);
             list = (List<SigPersonaldisponible>) query.list();
             
-            for (SigPersonaldisponible elem : list) {            
-                Disponible row = new Disponible();                              
+            for (SigPersonaldisponible elem : list) {
+                Disponible row = new Disponible();
                 row.setNombre(elem.getVcNmbempDisponible());
                 row.setApellido(elem.getVcApDisponible());
                 row.setArea(elem.getVcAreaDisponible());
                 row.setHoras(elem.getiHoraDisponible());
                 row.setMes(elem.getVcMesDisponible());
-                
+
                 lista.add(row);
 
             }
@@ -93,14 +63,69 @@ public class personalDisponibleBean implements Serializable{
             System.out.println(e.getMessage());
         } finally {
         }
-    
+
     }
-    
-    
-    
+
+    public List<SigPersonaldisponible> aniosTotales() {
+        Session session = null;
+        List<SigPersonaldisponible> list = null;
+        try {
+        session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("SELECT DISTINCT d.iAnioDisponible FROM SigPersonaldisponible d");
+        list = (List<SigPersonaldisponible>) query.list();
+        }catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        } finally {
+        }
+        
+        return list;
+    }
+
     /**
-     * Creates a new instance of personalDisponibleBean
+     * @return the mes
      */
-   
+    public String getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the anio
+     */
     
+
+    /**
+     * @return the lista
+     */
+    public List<Disponible> getLista() {
+        return lista;
+    }
+
+    /**
+     * @param lista the lista to set
+     */
+    public void setLista(List<Disponible> lista) {
+        this.lista = lista;
+    }
+
+    /**
+     * @return the anio
+     */
+    public Integer getAnio() {
+        return anio;
+    }
+
+    /**
+     * @param anio the anio to set
+     */
+    public void setAnio(Integer anio) {
+        this.anio = anio;
+    }
+
 }
